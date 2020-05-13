b8af06ecf18e16822588e5d82349baa05d3fcc45
SamuelMichaAssmann <s190165@student.dhbw-mannheim.de>
2020-05-13 14:37:23 +0200

    faq

---
+++ app/src/main/java/com/example/smarthomeupgrade/ui/html/NutzungsFragment.java
@@ -0,0 +1,31 @@
+package com.example.smarthomeupgrade.ui.html;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.smarthomeupgrade.R;
+
+public class NutzungsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HtmlViewModel htmlViewModel = ViewModelProviders.of(this).get(HtmlViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_html, container, false);
+
+        WebView webView = (WebView) root.findViewById(R.id.webview_home);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/stats.html");
+        WebSettings websettings = webView.getSettings();
+        websettings.setJavaScriptEnabled(true);
+
+        return root;
+    }
+}
